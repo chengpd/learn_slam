eff06f30ede9dd9f231311d3786c1aba3e4cbfe9
CP <d_scheng@163.com>
2022-09-12 16:09:10 +0800

    完善第二次文件

--- ch2/test1/main.cpp
+++ ch2/test1/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+/*求解 A * x = B 这个方程*/
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <iostream>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <Eigen/Cholesky>
+
+using namespace std;
+using namespace Eigen;
+
+#define MATRIX_SIZE 100
+
+int main( int argc,char** argv )
+{
+    MatrixXd A_pre = MatrixXd::Random( MATRIX_SIZE, MATRIX_SIZE );
+    MatrixXd A = A_pre.transpose()*A_pre ;                  //使得A为正定对称矩阵，才能使得cholesky分解成功
+    VectorXd B = VectorXd::Random( MATRIX_SIZE );
+    VectorXd x = A.colPivHouseholderQr().solve(B);          //调用QR分解求解
+    VectorXd y = A.llt().solve(B);                          //调用cholesky分解求解
+
+    cout <<"A*x=B方程的解为 \n"<< x.transpose() << endl;
+    cout <<"A*y=B方程的解为 \n"<< y.transpose() << endl;
 }
